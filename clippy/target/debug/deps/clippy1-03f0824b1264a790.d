85ce9ed7bab8b58f2629f547f9e52c665d2e836b
Antoni Stevent <itzmjauz@gmail.com>
2021-06-01 21:41:36 +0200

    quiz4done

---
+++ clippy/target/debug/deps/clippy1-03f0824b1264a790.d
@@ -0,0 +1,5 @@
+/home/itzmjauz/src/github.com/jakeisnt/rustlings-nix/exercises/clippy/target/debug/deps/clippy1-03f0824b1264a790.rmeta: clippy1.rs
+
+/home/itzmjauz/src/github.com/jakeisnt/rustlings-nix/exercises/clippy/target/debug/deps/clippy1-03f0824b1264a790.d: clippy1.rs
+
+clippy1.rs:
